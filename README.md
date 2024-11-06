d3bd77b29f4bbc2e47fb34cf03fede1ecb72d45f
Wh0Rv <salsabilacf04@icloud.com>
2024-11-06 09:17:46 +0700

    TUgas 7

--- README.md
+++ README.md
@@ -1,16 +1,30 @@
-# e_commerce
+**TUGAS 7**
+*Salsabila Caturizky Farindarmawan*
+*2306275664*
 
-A new Flutter project.
+1. Jelaskan apa yang dimaksud dengan stateless widget dan stateful widget, dan jelaskan perbedaan dari keduanya.
+    Stateless widget adalah widget yang tidak bisa berubah sealam aplikasi berjalan. 
+    Stateful widget adalah widget yang dapat berubah sesuai interaksi pengguna atau saat ada data yang diperbarui.
+    Perbedaan utamanya adalah stateless widget yang bersifat tetap dan stateful widget yang bisa berubap berdasarkan kondisi.
 
-## Getting Started
+2. Sebutkan widget apa saja yang kamu gunakan pada proyek ini dan jelaskan fungsinya.
+    Scaffold: Memberikan struktur dasar halaman aplikasi, seperti AppBar body dan floating action button.
+    AppBar: Untuk menampilkan bagian atas halaman, title (seperti navbar).
+    Text: Menampilkan teks di layar.
+    Icon: Menampilkan ikon yang sesuai dengan fungsi tombol di aplikasi.
+    ElevatedButton: Efek saat klik tombol.
+    GridView: Menampilkan elemen dalam bentuk grid.
+    Column dan Row: Mengatur susunan widget.
+    SnackBar: Menampilkan pesan di bawah layar setelah pengguna menekan tombol.
+    Padding: Menyediakan jarak sekitar widget.
+    Card: Menampilkan elemen sebagai kartu.
 
-This project is a starting point for a Flutter application.
+3. Apa fungsi dari setState()? Jelaskan variabel apa saja yang dapat terdampak dengan fungsi tersebut.
+    setState() berfungsi untuk menginformasikan Flutter bahwa adanya perubahan data atau kondisi yang perlu diupdate di layar. Variable yang terdampak dari setSatte() adalah variable yang state, seperti data dinamis, counter, status login.
 
-A few resources to get you started if this is your first Flutter project:
+4. Jelaskan perbedaan antara const dengan final.
+    Const digunakan untuk variable dengan nilai tetap tidak berubah atau telah diketahui dari awal.
+    Final digunakan untuk variable yang nilainya akan tetap setelah diinisialisasi, tapi baru ditentukan saat runtime, seperti dari input pengguna.
 
-- [Lab: Write your first Flutter app](https://docs.flutter.dev/get-started/codelab)
-- [Cookbook: Useful Flutter samples](https://docs.flutter.dev/cookbook)
-
-For help getting started with Flutter development, view the
-[online documentation](https://docs.flutter.dev/), which offers tutorials,
-samples, guidance on mobile development, and a full API reference.
+5. Jelaskan bagaimana cara kamu mengimplementasikan checklist-checklist di atas.
+    Membuat proyek dengan flutter create, mengubah widget jadi stateless, membuat tombol-tombol icon dan teks dengan membuat class itemhomepage dan menambahkan items pada myhomepage, menerapkan warna berbeda2 pada tombol dengan menambahkan logika pada itemcard, menampilkan snackbar pada ontap itemcard.
---
+++ lib/menu.dart
@@ -0,0 +1,173 @@
+import 'package:flutter/material.dart';
+
+class MyHomePage extends StatelessWidget {
+  final String npm = '2306275664';
+  final String name = 'Salsabila Caturizky Farindarmawan';
+  final String className = 'PBP B';
+
+  final List<ItemHomepage> items = [
+         ItemHomepage("Lihat Daftar Produk", Icons.list),
+         ItemHomepage("Tambah Produk", Icons.add),
+         ItemHomepage("Logout", Icons.logout),
+     ];
+
+  MyHomePage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Theme.of(context).colorScheme.primary,
+        title: const Text(
+          'Thrift Shop',
+          style: TextStyle(
+            color: Colors.white,
+            fontWeight: FontWeight.bold,
+          ),
+        ),
+      ),
+      body: Padding(
+        padding: const EdgeInsets.all(16.0),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: [
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+              children: [
+                InfoCard(title: 'NPM', content: npm),
+                InfoCard(title: 'Name', content: name),
+                InfoCard(title: 'Class', content: className),
+              ],
+            ),
+            const SizedBox(height: 16.0),
+
+            Center(
+              child: Column(
+                children: [
+                  const Padding(
+                    padding: EdgeInsets.only(top: 16.0),
+                    child: Text(
+                      'Welcome to Thrift Shop!',
+                      style: TextStyle(
+                        fontWeight: FontWeight.bold,
+                        fontSize: 18.0,
+                      ),
+                    ),
+                  ),
+
+                  // Grid untuk menampilkan ItemCard dalam bentuk grid 3 kolom.
+                  GridView.count(
+                    primary: true,
+                    padding: const EdgeInsets.all(20),
+                    crossAxisSpacing: 10,
+                    mainAxisSpacing: 10,
+                    crossAxisCount: 3,
+                    shrinkWrap: true,
+
+                    // Menampilkan ItemCard.
+                    children: items.map((ItemHomepage item) {
+                      return ItemCard(item);
+                    }).toList(),
+                  ),
+                ],
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+class InfoCard extends StatelessWidget {
+  final String title;
+  final String content;
+
+  const InfoCard({super.key, required this.title, required this.content});
+
+  @override
+  Widget build(BuildContext context) {
+    return Card(
+      // Kotak card dengan bayangan.
+      elevation: 2.0,
+      child: Container(
+        width: MediaQuery.of(context).size.width / 3.5,
+        padding: const EdgeInsets.all(16.0),
+        child: Column(
+          children: [
+            Text(
+              title,
+              style: const TextStyle(fontWeight: FontWeight.bold),
+            ),
+            const SizedBox(height: 8.0),
+            Text(content),
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+class ItemHomepage {
+    final String name;
+    final IconData icon;
+
+    ItemHomepage(this.name, this.icon);
+}
+
+class ItemCard extends StatelessWidget {
+  final ItemHomepage item; 
+  
+  const ItemCard(this.item, {super.key}); 
+
+  @override
+  Widget build(BuildContext context) {
+    Color buttonColor;
+    if (item.name == "Lihat Daftar Produk") {
+      buttonColor = Colors.blue;
+    } else if (item.name == "Tambah Produk") {
+      buttonColor = Colors.green;
+    } else if (item.name == "Logout") {
+      buttonColor = Colors.red;
+    } else {
+      buttonColor = Theme.of(context).colorScheme.secondary;
+    }
+
+    return Material(
+      color: buttonColor,
+      borderRadius: BorderRadius.circular(12),
+      child: InkWell(
+        onTap: () {
+          // Menampilkan pesan SnackBar saat card ditekan.
+          ScaffoldMessenger.of(context)
+            ..hideCurrentSnackBar()
+            ..showSnackBar(
+              SnackBar(content: Text("Kamu telah menekan tombol ${item.name}!"))
+            );
+        },
+        // Container untuk menyimpan Icon dan Text
+        child: Container(
+          padding: const EdgeInsets.all(8),
+          child: Center(
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Icon(
+                  item.icon,
+                  color: Colors.white,
+                  size: 30.0,
+                ),
+                const Padding(padding: EdgeInsets.all(3)),
+                Text(
+                  item.name,
+                  textAlign: TextAlign.center,
+                  style: const TextStyle(color: Colors.white),
+                ),
+              ],
+            ),
+          ),
+        ),
+      ),
+    );
+  }  
+}
